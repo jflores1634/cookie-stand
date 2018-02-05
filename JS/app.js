a9464f00f1fc8e49a901342f38482a95d99fe7ec
Jose Flores <jose.flores1634@gmail.com>
2018-02-04 23:55:11 -0800

    created the form and added styling

--- JS/app.js
+++ JS/app.js
@@ -3,7 +3,7 @@
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 var cookStore = [];
 var cookieTable = document.getElementById('cookies');
-
+var cookieForm = document.getElementById('cookie-form');
 
 function PatsCookies(name, min, max, avg) {
   this.name = name;
@@ -12,13 +12,12 @@ function PatsCookies(name, min, max, avg) {
   this.avg = avg;
   this.cookiesEachHour = [];
   this.totalDailySales = 0;
-  cookStore.push(this);
-  this.calcCookiesEachHour();
+  PatsCookies.cookStore.push(this);
 }
 
 PatsCookies.prototype.calcCookiesEachHour = function() {
   for(var i = 0; i < hours.length; i++) {
-    var oneHour = Math.floor(this.calcCustsEachHour() * this.avg);
+    var oneHour = Math.floor(Math.random(this.calcCustsEachHour() * this.avg));
     this.cookiesEachHour.push(oneHour);
     this.totalDailySales += oneHour;
   }
@@ -41,6 +40,13 @@ PatsCookies.prototype.render = function(){
   trEl.appendChild(tdEl);
   cookieTable.appendChild(trEl);
 };
+
+function renderStores() {
+  for(var i = 0; i < cookStore.length; i++) {
+  cookStore[i].cookiesEachHour.length = 0;
+  cookStore[i].render();
+ }
+}
 
 // create a for loop for an array for the time slots instead of whats below, make global array at top
 function headRow() {
@@ -68,7 +74,20 @@ function headRow() {
     cookieTable.appendChild(trEl);
   }
 
+  function addNewStore(event) {
+    event.preventDefault();
+    var newStore = event.target.newStore.value;
+    var newMin = parseInt(event.target.newMin.value);
+    var newMax = parseInt(event.target.newMax.value);
+    var newAvg = event.target.newAvg.value;
 
+    new PatsCookies(newStore, newMin, newMax, newAvg);
+
+    cookieTable.innerHTML = '';
+    headRow();
+    renderStores();
+    footRow();
+  }
 
   var pikePlaceMarket = new PatsCookies('1st and Pike', 23, 65, 6.3);
   var seatacAirport = new PatsCookies('Seatac Airport', 23, 65, 6.3);
@@ -76,11 +95,8 @@ function headRow() {
   var capitolHill = new PatsCookies('Capitol Hill', 23, 65, 6.3);
   var alkiBeach = new PatsCookies('Alki Beach', 23, 65, 6.3);
 
+   cookieForm.addEventlistener('submit', addNewStore);
 
   headRow();
-  pikePlaceMarket.render();
-  seatacAirport.render();
-  seattleCenter.render();
-  capitolHill.render();
-  alkiBeach.render();
+  renderStores();
   footRow();
