998ab21ea2cd24677dfbcd89a34e212c5f0c9531
Jose Flores <jose.flores1634@gmail.com>
2018-02-04 23:28:55 -0800

    created the constructor, added styling, font

--- JS/app.js
+++ JS/app.js
@@ -4,47 +4,49 @@ var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
 var cookStore = [];
 var cookieTable = document.getElementById('cookies');
 
-function patsCookies(name, min, max, avg,) {
+
+function PatsCookies(name, min, max, avg) {
   this.name = name;
   this.min = min;
   this.max = max;
   this.avg = avg;
-  this.cookiesEachHour: [];
-  this.totalDailySales: 0;
-  allCookies.push(this);
+  this.cookiesEachHour = [];
+  this.totalDailySales = 0;
+  cookStore.push(this);
+  this.calcCookiesEachHour();
 }
 
-patsCookies.prototype.calcCookiesEachHour = function() {
-    var custsEachHour = Math.floor(Math.random() * (this.max - this.min) + this.min);
-    return Math.floor(this.avg * custsEachHour);
-  }
-
-patsCookies.prototype.calcCustsEachHour = function() {
-  for(var i = 0; i < hours.length; i++){
-    var oneHour = Math.floor(this.custsEachHour[i] * this.avg);
+PatsCookies.prototype.calcCookiesEachHour = function() {
+  for(var i = 0; i < hours.length; i++) {
+    var oneHour = Math.floor(this.calcCustsEachHour() * this.avg);
     this.cookiesEachHour.push(oneHour);
     this.totalDailySales += oneHour;
   }
+  return this.cookiesEachHour;
 };
 
-render: function(){
+PatsCookies.prototype.render = function(){
   this.calcCookiesEachHour();
   var trEl = document.getElementById('tr');
-  var tdEl = document.createElement('td');
-    tdEl.textContent = hours[i] + ': ' + this.cookiesEachHour[i] + ' cookies';
+  var thEl = document.createElement('th');
+  for(var i = 0; i < hours.length; i++){
+    tdEl.textContent = this.name;
+    trEl.appendChild(thEl);
+    var tdEl = document.createElement('td');
+    tdEl.textContent = this.cookiesEachHour[i] + ' cookies';
     trEl.appendChild(tdEl);
-    for(var i = 0; i < hours.length; i++){
+  }
   tdEl = document.createElement('td');
-  tdEl.textContent = 'Total: ' + this.totalDailySales + ' cookies';
-  trEl.appendChild(liEl);
-}
+  tdEl.textContent = this.totalDailySales;
+  trEl.appendChild(tdEl);
+  cookieTable.appendChild(trEl);
 };
 
 // create a for loop for an array for the time slots instead of whats below, make global array at top
 function headRow() {
   var trEl = document.createElement('tr');
+  var thEl = document.createElement('th');
   for(var i = 0; i < hours.length; i++){
-    var thEl = document.createElement('th');
     thEl.textContent = hours[i];
     trEl.appendChild(thEl);
     cookieTable.appendChild(trEl);
@@ -57,24 +59,22 @@ function headRow() {
     trEl.appendChild(thEl);
     for(var i = 0; i < hours.length; i++) {
       var hourlyTotal = 0;
-
-      for(var j = 0; j < cookStore.length; i++){
+      var tdEl = document.createElement('td');
+      for(var j = 0; j < cookStore.length; j++){
         hourlyTotal += cookStore.length[j].cookiesEachHour[i];
-
+        thEl.textContent = hourlyTotal;
+        trEl.appendChild(tdEl);
       }
-      thEl = document.createElement('th');
-      thEl.textContent = hourlyTotal;
-      trEl.appendChild(thEl);
-    }
-
     cookieTable.appendChild(trEl);
   }
 
-  var pikePlaceMarket = new patsCookies('1st and Pike', 23, 65, 6.3);
-  var seatacAirport = new patsCookies('Seatac Airport', 23, 65, 6.3);
-  var seattleCenter = new patsCookies('Seattle center', 23, 65, 6.3);
-  var capitolHill = new patsCookies('Capitol Hill', 23, 65, 6.3);
-  var alkiBeach = new patsCookies('Alki Beach', 23, 65, 6.3);
+
+
+  var pikePlaceMarket = new PatsCookies('1st and Pike', 23, 65, 6.3);
+  var seatacAirport = new PatsCookies('Seatac Airport', 23, 65, 6.3);
+  var seattleCenter = new PatsCookies('Seattle center', 23, 65, 6.3);
+  var capitolHill = new PatsCookies('Capitol Hill', 23, 65, 6.3);
+  var alkiBeach = new PatsCookies('Alki Beach', 23, 65, 6.3);
 
 
   headRow();
